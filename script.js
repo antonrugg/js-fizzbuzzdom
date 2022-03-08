a1c2bbfdb717451a95f0d19ba7af29fa801fff6d
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-08 15:29:41 +0100

    adding comments

--- script.js
+++ script.js
@@ -2,20 +2,21 @@
 
 //create a div container in dom, append an element in html with the correct index and add different styles for 3, 5 and both multiples
 
+//declaring variables
 const fizz = 'fizz';
 const buzz = 'buzz';
 
 
 const containerSquare = document.querySelector(".container");
-
-
+//for cicle from 1 to 100
 for (let i = 1; i <= 100; i++){
-  
+  //logic to check when mutiples of 3,5 and both
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(`${fizz}${buzz}`);
-        
+        //adding element in dom: div with id and class, containing text with variables
         const element = `<div id="square-${i}" class="square">${fizz}${buzz}</div>`;
         containerSquare.innerHTML += element;
+        //adding style to single elements
         document.getElementById('square-' + i).style.backgroundColor = "red";
         
     } else if(i % 5 === 0){
