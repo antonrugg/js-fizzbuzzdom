306a64f27735d2649f17c217f3a42455798db354
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-08 13:51:46 +0100

    console log fizz, buzz or fizzbuzz multiples of 3,5 or both

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-//console log numbers from 1 to 100
+//console log numbers from 1 to 100, if multiple of 3 log 'fizz', if multiple of 5 log 'buzz', if multiple of both 'fizzbuzz'
 
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz');
+    } else if(i % 5 === 0){
+        console.log('buzz');
+    } else if (i % 3 === 0) {
+        console.log('fizz');
+    } else {
+        console.log(i);
+    }
 }
 
